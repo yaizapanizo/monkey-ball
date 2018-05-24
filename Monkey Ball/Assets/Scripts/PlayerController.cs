2bf9daf20bff00323e215fa9d515ac049df6464d
yaizapanizo <yaiza.panizo@gmail.com>
2018-05-24 16:49:23 +0200

    Banana counter caught
    Counter on the top-left-side of the screen

--- Monkey Ball/Assets/Scripts/PlayerController.cs
+++ Monkey Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
 	void FixedUpdate ()
@@ -27,6 +32,13 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
